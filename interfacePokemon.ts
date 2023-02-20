e3e8e46fb5ee49062675a3a2e55ddc90290b1960
Francisco Zamudio <paco991121@hotmail.com>
2023-02-20 10:13:59 -0600

    Ya se reciben y muestran todos los datos

---
+++ interfacePokemon.ts
@@ -0,0 +1,11 @@
+interface interfacePokemon {
+    name: string;
+    types: string;
+    description: string;
+    noDamageTo: string;
+    halfDamageTo: string;
+    doubleDamageTo: string;
+    doubleDamageFrom: string;
+    moves: string;
+    evolutions: string;
+  }
